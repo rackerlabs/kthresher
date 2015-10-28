3d937f16f25d3b277e3210376307067665f926dd
Tony Garcia <tony.garcia@rackspace.com>
2015-10-28 11:13:14 -0500

    Formatting.

--- setup.py
+++ setup.py
@@ -17,4 +17,15 @@ setuptools.setup(
     url='https://github.com/rackerlabs/kthresher',
     download_url='https://github.com/rackerlabs/kthresher/tarball/0.2.1',
     zip_safe=False
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Environment :: Console',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
 )
